72dae7c326d67ddfe7469c9175234fd521afc08e
DAKSHINA MOORTHY N D <nddakshinamoorthy07@gmail.com>
2025-07-16 18:06:20 +0530

    Delete Brands_app/cloud_kitchen_project_updated directory

--- Brands_app/cloud_kitchen_project_updated/brands/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Brand
-
-@admin.register(Brand)
-class BrandAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'kitchen', 'description')
